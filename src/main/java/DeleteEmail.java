d67c34c41e7c8df527d4abcd5ff2daa6be2e62b9
Blag <atejada@gmail.com>
2023-12-21 10:47:03 -0500

    Update DeleteEmail.java

--- src/main/java/DeleteEmail.java
+++ src/main/java/DeleteEmail.java
@@ -1,45 +1,18 @@
-//Import Java Utilities
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-//Import Nylas Packages
-import com.nylas.RequestFailedException;
-import com.nylas.NylasAccount;
+// Import Nylas packages
 import com.nylas.NylasClient;
-import com.nylas.Label;
-import com.nylas.LabelQuery;
-import com.nylas.Labels;
-import com.nylas.Messages;
-import com.nylas.Message;
 
-//Import DotEnv to handle .env files
+// Import DotEnv to handle .env files
+import com.nylas.models.*;
 import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvException;
 
 public class DeleteEmail {
-    public static void main(String[] args) throws RequestFailedException, IOException {
+    public static void main(String[] args) throws NylasSdkTimeoutError, NylasApiError {
+        // Load the .env file
         Dotenv dotenv = Dotenv.load();
-        // Create the client object
-        NylasClient client = new NylasClient();
-        // Connect it to Nylas using the Access Token from the .env file
-        NylasAccount account = client.account(dotenv.get("ACCESS_TOKEN"));
+        // Initialize the Nylas client
+        NylasClient nylas = new NylasClient.Builder(dotenv.get("V3_TOKEN_API")).build();
 
-        String messageId = "{message_id}";
-        Map<String, String> map = new HashMap<String, String>();
-        Messages messages = account.messages();
-        Labels labels = account.labels();
-        for(Label label : labels.list()){
-            map.put(label.getName(), label.getId());
-        }
-
-        try{
-            messages.setLabelIds(messageId, Arrays.asList(map.get("trash")));
-            System.out.println("Message was successfully deleted");
-        }catch (Exception e){
-            System.out.println("Message couldn't be deleted");
-        }
-
+        DeleteResponse response = nylas.messages().destroy(dotenv.get("GRANT_ID"), "{MESSAGE_ID}");
+        System.out.println(response);
     }
 }
